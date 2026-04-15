2630a084192461a9790813f6a4b9d7e0dc7ee3e9
Hasan Md Zakaria <zakariak4@gmail.com>
2026-04-15 23:28:52 +0600

    add Navbar with responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import RootLayout from './layout/RootLayout'
+import HomePage from './pages/HomePage/HomePage'
+import TimeLine from './pages/TimeLinePage/TimeLine'
+import Stats from './pages/StatsPage/Stats'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+       index:true,
+       Component: HomePage,
+      },
+      {
+       path:"/timeline",
+       Component: TimeLine,
+      },
+      {
+       path:"/stats",
+       Component: Stats,
+      },
+    ],
+    errorElement: <h2>This page is not found</h2>
+  },
+]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h2 className='bg-white text-black'>Welcome to keen keeper project!</h2>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/pages/StatsPage/Stats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats = () => {
+    return (
+        <div>
+            StatsPage
+        </div>
+    );
+};
+
+export default Stats;
