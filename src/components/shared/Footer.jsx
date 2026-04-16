d097517b64028742341c79dd1576c0ec52564339
Hasan Md Zakaria <zakariak4@gmail.com>
2026-04-16 18:48:36 +0600

    fetch data from friends data json and desgn friend card with responsive all device

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,49 +1,62 @@
-import React from 'react';
-import FooterLogo from '../../assets/logo-xl.png';
-import Instagram from '../../assets/instagram.png';
-import Facebook from '../../assets/facebook.png';
-import Twitter from '../../assets/twitter.png';
+import React from "react";
+import FooterLogo from "../../assets/logo-xl.png";
+import Instagram from "../../assets/instagram.png";
+import Facebook from "../../assets/facebook.png";
+import Twitter from "../../assets/twitter.png";
 
 const Footer = () => {
-    return (
-        <div className='bg-[#244D3F] mt-16 md:mt-20'>
+  return (
+    <div className="bg-[#244D3F] mt-16 md:mt-20">
+      <div className="max-w-277.5 mx-auto px-4 pt-12 md:pt-16 lg:pt-20 pb-8 md:pb-10 border-b border-gray-600 flex flex-col items-center text-center">
+        <img src={FooterLogo} alt="Footer-logo" className="w-40 md:w-48" />
 
-            {/* Top Section */}
-            <div className='max-w-277.5 mx-auto px-4 pt-12 md:pt-16 lg:pt-20 pb-8 md:pb-10 border-b border-gray-600 flex flex-col items-center text-center'>
-                
-                <img src={FooterLogo} alt="Footer-logo" className='w-40 md:w-48' />
+        <p className="text-white mt-4 text-sm md:text-base max-w-150">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </p>
 
-                <p className='text-white mt-4 text-sm md:text-base max-w-150'>
-                    Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
-                </p>
+        <h3 className="text-lg md:text-[20px] font-medium text-white mt-6">
+          Social Links
+        </h3>
 
-                <h3 className='text-lg md:text-[20px] font-medium text-white mt-6'>
-                    Social Links
-                </h3>
-
-                <div className='flex gap-4 mt-4'>
-                    <img src={Instagram} alt="Instagram-logo" className='w-6 h-6 md:w-7 md:h-7 cursor-pointer' />
-                    <img src={Facebook} alt="Facebook-logo" className='w-6 h-6 md:w-7 md:h-7 cursor-pointer' />
-                    <img src={Twitter} alt="Twitter-logo" className='w-6 h-6 md:w-7 md:h-7 cursor-pointer' />
-                </div>
-            </div>
-
-            {/* Bottom Section */}
-            <div className='max-w-277.5 mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4 py-4 text-center md:text-left'>
-                
-                <p className='text-[#FAFAFA] text-sm md:text-base'>
-                    © 2026 KeenKeeper. All rights reserved.
-                </p>
-
-                <div className='flex flex-wrap justify-center md:justify-end gap-4 md:gap-6'>
-                    <p className='text-[#FAFAFA] text-sm md:text-base cursor-pointer'>Privacy Policy</p>
-                    <p className='text-[#FAFAFA] text-sm md:text-base cursor-pointer'>Terms of Service</p>
-                    <p className='text-[#FAFAFA] text-sm md:text-base cursor-pointer'>Cookies</p>
-                </div>
-            </div>
-
+        <div className="flex gap-4 mt-4">
+          <img
+            src={Instagram}
+            alt="Instagram-logo"
+            className="w-6 h-6 md:w-7 md:h-7 cursor-pointer"
+          />
+          <img
+            src={Facebook}
+            alt="Facebook-logo"
+            className="w-6 h-6 md:w-7 md:h-7 cursor-pointer"
+          />
+          <img
+            src={Twitter}
+            alt="Twitter-logo"
+            className="w-6 h-6 md:w-7 md:h-7 cursor-pointer"
+          />
         </div>
-    );
+      </div>
+
+      <div className="max-w-277.5 mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4 py-4 text-center md:text-left">
+        <p className="text-[#FAFAFA] text-sm md:text-base">
+          © 2026 KeenKeeper. All rights reserved.
+        </p>
+
+        <div className="flex flex-wrap justify-center md:justify-end gap-4 md:gap-6">
+          <p className="text-[#FAFAFA] text-sm md:text-base cursor-pointer">
+            Privacy Policy
+          </p>
+          <p className="text-[#FAFAFA] text-sm md:text-base cursor-pointer">
+            Terms of Service
+          </p>
+          <p className="text-[#FAFAFA] text-sm md:text-base cursor-pointer">
+            Cookies
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/shared/HomePage/Friends.jsx
+++ src/components/shared/HomePage/Friends.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import FriendCard from '../../ui/FriendCard';
+import { CircleLoader} from 'react-spinners';
 
 const Friends = () => {
+
+    const [friends, setFriends] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const res = await fetch('/data.json');
+            const data = await res.json();
+            //console.log(data);
+            setTimeout(() => {
+                setFriends(data.friends);
+                setLoading(false);
+            }, 1500);
+        }
+        fetchData();
+    },[]);
+
+    console.log(friends, "Friends");
+    console.log(loading, "loading");
+
+
     return (
-        <div className='max-w-277.5 mx-auto mt-10'>
-            <h2 className='text-[24px] font-semibold text-[#1F2937]'>Your Friend</h2>
+        <div className='max-w-277.5 mx-auto mt-10 px-4'>
+            <h2 className='text-[24px] font-semibold text-[#1F2937]text-center md:text-left'>Your Friend</h2>
+
+            {loading ? (
+                <div className='flex justify-center items-center'>
+                    <CircleLoader color="#244D3F"/>
+                </div>
+            ) : (
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10'>
+                {
+                    friends.map(friend => {
+                        return <FriendCard key={friend.id} friend={friend}/>
+                    })
+                }
+            </div>
+            )}
         </div>
     );
 };
